3026e2943a4405beadd412c1a875dcf23509498f
Mikołaj Chojnacki <mikolaj.chojnacki@wp.pl>
2022-11-17 17:14:43 +0100

    Add form validation

--- src/Components/Features/PostForm/PostForm.js
+++ src/Components/Features/PostForm/PostForm.js
@@ -1,9 +1,11 @@
 import { useState } from 'react';
+import { useForm } from "react-hook-form";
 import ReactQuill from 'react-quill';
 import DatePicker from "react-datepicker";
 
 import "react-datepicker/dist/react-datepicker.css";
 import 'react-quill/dist/quill.snow.css';
+import { Form } from 'react-bootstrap';
 
 const PostForm = ({ action, actionText, ...props }) => {
 
@@ -12,34 +14,65 @@ const PostForm = ({ action, actionText, ...props }) => {
     const [publishedDate, setPublishedDate] = useState(props.publishedDate || new Date());
     const [shortDescription, setShortDescription] = useState(props.shortDescription || '');
     const [content, setContent] = useState(props.content || '');
+    const [contentError, setContentError] = useState(false);
+    const [dateError, setDateError] = useState(false);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        action({ title, author, publishedDate, shortDescription, content });
-    }
+
+    const { register, handleSubmit: validate, formState: { errors } } = useForm();
+
+    // const handleSubmit = (e) => {
+    //     action({ title, author, publishedDate, shortDescription, content });
+    // }
+
+    const handleSubmit = () => {
+        setContentError(!content)
+        setDateError(!publishedDate)
+        if (content && publishedDate) {
+            action({ title, author, publishedDate, shortDescription, content });
+        }
+    };
 
     return (
-        <form className="col-10 col-sm-10 col-md-8  col-lg-6 " onSubmit={handleSubmit}>
+        <form className="col-10 col-sm-10 col-md-8  col-lg-6 " onSubmit={validate(handleSubmit)}>
             <div className="form-group d-flex flex-column col-10  col-sm-6">
                 <label className={`p-2`} >Title</label>
-                <input className={`form-control`} value={title} onChange={e => setTitle(e.target.value)} type="text" />
+                <Form.Control
+                    {...register("title", { required: true, minLength: 3 })}
+                    value={title}
+                    onChange={e => setTitle(e.target.value)}
+                    type="text" placeholder="Enter title"
+                />
+                {errors.title && <small className="d-block form-text text-danger mt-2">This field is required</small>}
             </div>
             <div className="form-group d-flex flex-column  col-10  col-sm-6">
                 <label className={`p-2`} >Author</label>
-                <input className={`form-control`} value={author} onChange={e => setAuthor(e.target.value)} type="text" />
+                <Form.Control
+                    {...register("author", { required: true, minLength: 3 })}
+                    value={author}
+                    onChange={e => setAuthor(e.target.value)}
+                    type="text" placeholder="Enter author"
+                />
+                {errors.author && <small className="d-block form-text text-danger mt-2">This field is required</small>}
             </div>
             <div className="form-group d-flex flex-column  col-10  col-sm-6">
                 <label className={`p-2`} >Published</label>
                 <DatePicker selected={publishedDate} onChange={(date) => setPublishedDate(date)} />
-                {/* <input className={`form-control`} value={publishedDate} onChange={e => setPublishedDate(e.target.value)} type="text" /> */}
+                {dateError && <small className="d-block form-text text-danger mt-2">Wrong date</small>}
             </div>
             <div className="form-group d-flex flex-column  col-12  col-sm-8">
                 <label className={`p-2`} >Short description</label>
-                <ReactQuill theme="snow" value={shortDescription} onChange={setShortDescription} />
+                <Form.Control
+                    {...register("shortDescription", { required: true, minLength: 20 })}
+                    value={shortDescription}
+                    onChange={e => setShortDescription(e.target.value)}
+                    type="text" placeholder="Short description"
+                />
+                {errors.shortDescription && <small className="d-block form-text text-danger mt-2">This field is required</small>}
             </div>
             <div className="form-group d-flex flex-column  col-12  col-sm-8">
                 <label className={`p-2`} >Main content</label>
                 <ReactQuill theme="snow" value={content} onChange={setContent} />
+                {contentError && <small className="d-block form-text text-danger mt-2">Content can't be empty</small>}
             </div>
             <button className={`btn btn-primary mt-3`}>{actionText}</button>
         </form>
